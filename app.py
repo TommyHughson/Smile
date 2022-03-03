fbea164c71816505244fd29a6e089a9d802f7536
TommyHughson <18217@wc.school.nz>
2022-03-03 15:02:19 +1300

    Added menu from database

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
+import sqlite3
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+DATABASE = r"C:\Users\18217\OneDrive - Wellington College\13DTS\Smile\smile.db"
+
+
+def create_connection(db_file):
+    """
+    Create a connection with the database
+    parameter: name of the database file
+    returns: a connection to the file
+    """
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except sqlite3.Error as e:
+        print(e)
+        return None
 
 
 @app.route('/')
@@ -10,7 +27,14 @@ def render_homepage():
 
 @app.route('/menu')
 def render_menu_page():
-    return render_template('menu.html')
+    con = create_connection(DATABASE)
+
+    query = "SELECT name, description, volume, price, image FROM product"
+    cur = con.cursor()  # creates a courser to write the query
+    cur.execute(query)
+    product_list = cur.fetchall()
+    con.close()
+    return render_template('menu.html', products=product_list)
 
 
 @app.route('/contact')
